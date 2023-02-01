9bcbf5aa65042ed83103281b8e16b5d274cc0a4c
Ayşegül Aydın <94490342+ayseglaydnn@users.noreply.github.com>
2023-02-01 19:42:09 +0300

    PasswordGenerator v.1

---
+++ Projects/Project1_SimplePasswordGenerator/CharacterManager.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordCreator.Console
+{
+    public class CharacterManager
+    {
+        public List<Character> CharacterLists { get; set; }
+        private List<Character> SelectedCharacters { get; set; }
+
+        private Random _random;
+        public CharacterManager()
+        {
+            CharacterLists = new List<Character>();
+            _random = new Random();
+            SelectedCharacters = new List<Character>();
+        }
+
+        public List<Character> AddList(string includeStatus, string includeType)
+        {
+
+            if (includeStatus == "y" || includeStatus == "Y")
+            {
+                foreach (var x in includeType)
+                {
+                    Character character = new Character()
+                    {
+                        character = x
+                    };
+                    CharacterLists.Add(character);
+                }
+            }
+            return CharacterLists;
+        }
+
+        public int CharactersCount => CharacterLists.Count();
+
+        public char GetRandomCharacter()
+        {
+            var randomIndex = _random.Next(0, CharactersCount); // get random value between 0 and length of character list 
+            var RandomCharacter = CharacterLists[randomIndex];
+            return RandomCharacter.character;
+        }
+
+        public string GetRandomPassword(int PasswordLength)
+        {
+            var password = new char[PasswordLength];
+            for (int i = 0; i < PasswordLength; i++)
+            {
+                password[i] = Convert.ToChar(GetRandomCharacter());
+            }
+            string GeneratedPassword = string.Join("", password);
+
+            return GeneratedPassword;
+        }
+    }
+}
---
+++ Projects/Project1_SimplePasswordGenerator/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Graph;
+using PasswordCreator.Console;
+using System;
+
+Console.WriteLine("\n****** Welcome to the Aysegul's Password Generator! ******\n");
+Console.WriteLine("Do you want to include Numbers in your password? Enter y for yes and anything else for no.\n");
+var includeNumberStatus = Console.ReadLine();
+Console.WriteLine("Do you want to include Lowercase Characters in your password? Enter y for yes and anything else for no.\n");
+var includeLowerCharStatus = Console.ReadLine();
+Console.WriteLine("Do you want to include Uppercase Characters in your password? Enter y for yes and anything else for no.\n");
+var includeUpperCharStatus = Console.ReadLine();
+Console.WriteLine("Do you want to include Special Characters in your password? Enter y for yes and anything else for no.\n");
+var includeSpecialCharStatus = Console.ReadLine();
+Console.WriteLine("How many characters do you want your password to have?\n");
+int PasswordLength = Convert.ToInt32(Console.ReadLine());
+
+String Numbers = "0123456789";
+String LowerChars = "abcdefghijklmnopqrstuvwxyz";
+String UpperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+String SpecialChar = "!\"#$%&'()*+,‑./:<>=?@";
+
+var characterManager = new CharacterManager();
+
+var CharacterList = characterManager.AddList(includeNumberStatus, Numbers);
+CharacterList = characterManager.AddList(includeLowerCharStatus, LowerChars);
+CharacterList = characterManager.AddList(includeUpperCharStatus, UpperChars);
+CharacterList = characterManager.AddList(includeSpecialCharStatus, SpecialChar);
+
+Console.WriteLine(characterManager.GetRandomPassword(PasswordLength));
+
+
