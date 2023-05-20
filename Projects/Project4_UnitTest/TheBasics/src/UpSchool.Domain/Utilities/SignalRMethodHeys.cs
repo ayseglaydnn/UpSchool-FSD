8757460be06f18aef1b52fcfe8079a0af469e6b1
Ayşegül Aydın <aysegul.416.aydin@gmail.com>
2023-05-20 18:36:04 +0300

    The Basics project were added.

---
+++ Projects/Project4_UnitTest/TheBasics/src/UpSchool.Domain/Utilities/SignalRMethodHeys.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpSchool.Domain.Utilities
+{
+    public static class SignalRMethodHeys
+    {
+        public static class Accounts
+        {
+            public static string Added => nameof(Added);
+            public static string DeleteAsync => nameof(DeleteAsync);
+            public static string Deleted => nameof(Deleted);
+
+        }
+        
+    }
+}
